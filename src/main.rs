3d5da64d3ed61fb50d163f7be60fbdc875513a09
cookju <epicdevelopers26@gmail.com>
2024-04-10 12:46:09 -0400

    mut ref

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use std::fs::File;
 use std::io::BufReader;
 use std::error::Error;
 
+use rbx_types::Ref;
+
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
 
 fn test(file_name : String) -> Result<(), Box<dyn Error>> {
+
     // Using buffered I/O is recommended with rbx_binary
     let input = BufReader::new(File::open(file_name)?);
 
@@ -15,45 +18,42 @@ fn test(file_name : String) -> Result<(), Box<dyn Error>> {
     // To get to the instances from our file, we need to go one level deeper.
     let mut dom = rbx_binary::from_reader(input)?;
 
-    let mut q: Queue<rbx_dom_weak::Instance> = queue![];
+    let mut vec : Vec<Ref> = Vec::new();
+
+    fn find_scripts(dom1 : &rbx_dom_weak::WeakDom, parent : &rbx_dom_weak::Instance, vec1 : &mut Vec<Ref>) {
+        for &referent in parent.children() {
+            let instance = dom1.get_by_ref(referent).unwrap();
+
+            if instance.class == "Script" || instance.class == "LocalScript" {
+
+                vec1.push(referent);
+
+                for (key, value) in &instance.properties {
+                    let source;
+                    if key == "Source" {
+                        source = value;
+                    }
+
+                    println!("  {}: {:?}", key, value);
+                }
+            }
+
+            find_scripts(dom1, instance, vec1);
+        }
+    }
 
     print_type_of(&dom);
 
-    let root = dom.root_mut();
+    let root = dom.root();
 
-    for &referent in parent.children() {
+    find_scripts(&dom, root, &mut vec);
+
+    for referent in vec.iter_mut() {
+        let instance = dom.get_by_ref_mut(*referent).unwrap();
+        println!("{}", instance.name);
+
 
     }
-
-    // fn find_scripts(dom1 : &mut rbx_dom_weak::WeakDom, parent : &mut rbx_dom_weak::Instance) {
-    //     for &referent in parent.children() {
-    //         let instance = dom1.get_by_ref_mut(referent).unwrap();
-
-    //         instance.name = "test".to_string();
-
-    //         println!("- {}", instance.name);
-
-    //         if instance.class == "Script" || instance.class == "LocalScript" {
-
-    //             for (key, value) in &instance.properties {
-    //                 let source;
-    //                 if key == "Source" {
-    //                     source = value;
-    //                 }
-
-    //                 //println!("  {}: {:?}", key, value);
-    //             }
-    //             //println!(" ");
-    //         }
-
-    //         find_scripts(dom1, instance);
-
-    //     }
-    // }
-
-
-
-    //find_scripts(&mut dom, root);
 
     Ok(())
 }
